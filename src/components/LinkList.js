49f99f92565953a78fae4c8b46faec86fcccdbde
Chris Mears <chris.mears@gmail.com>
2018-01-05 16:36:54 -0800

    Initial MVP commit

---
+++ src/components/LinkList.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import Link from './Link'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class LinkList extends Component {
+  render() {
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.allLinksQuery.allLinks
+
+    return(
+      <div>
+        {linksToRender.map((link, index) => (
+          <Link
+            key={link.id}
+            updateStoreAfterVote={this._updateCacheAfterVote}
+            index={index}
+            link={link}
+          />
+        ))}
+      </div>
+    )
+  }
+
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+    const votedLink = data.allLinks.find(link => link.id === linkId)
+    votedLink.votes = createVote.link.votes
+    store.writeQuery({ query: ALL_LINKS_QUERY, data })
+  }
+}
+
+export const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList)
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link';
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker';
+import { GC_AUTH_TOKEN } from './constants';
+import './styles/index.css'
+
+const httpLink = new HttpLink({ uri: 'http://localhost:60000/simple/v1/cjc126wcm001f0111mes33g56' })
+
+const middlewareAuthLink = new ApolloLink( (operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN)
+  const authorizationHeader = token ? `Bearer ${token}` : null
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  })
+  return forward(operation)
+})
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
+
+const client = new ApolloClient({
+  link: httpLinkWithAuthToken,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker();
