56f7b96192a38519f170d4e10a015539dc48aa45
Chris Mears <chris.mears@gmail.com>
2018-01-08 12:53:54 -0800

    Add subscriptions for new Links and Votes 📚

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloLink } from 'apollo-link';
+import { split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import App from './components/App'
 import registerServiceWorker from './registerServiceWorker';
 import { GC_AUTH_TOKEN } from './constants';
@@ -26,8 +29,27 @@ const middlewareAuthLink = new ApolloLink( (operation, forward) => {
 
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink)
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:60000/subscriptions/v1/cjc126wcm001f0111mes33g56`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN)
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken
+)
+
 const client = new ApolloClient({
-  link: httpLinkWithAuthToken,
+  link,
   cache: new InMemoryCache()
 })
 
