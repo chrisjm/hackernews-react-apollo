843490f2e82bf542cfbb9744fdb40f4f1ede3f12
Chris Mears <chris.mears@gmail.com>
2018-01-09 11:03:15 -0800

    Add pagination 📑

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Link from './Link'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import { LINKS_PER_PAGE } from '../constants'
 
 class LinkList extends Component {
   componentDidMount() {
@@ -18,20 +19,55 @@ class LinkList extends Component {
       return <div>Error</div>
     }
 
-    const linksToRender = this.props.allLinksQuery.allLinks
+    const isNewPage = this.props.location.pathname.includes('new')
+    const linksToRender = this._getLinksToRender(isNewPage)
+    const page = parseInt(this.props.match.params.page, 10)
 
     return(
       <div>
+        <div>
         {linksToRender.map((link, index) => (
           <Link
             key={link.id}
-            updateStoreAfterVote={this._updateCacheAfterVote}
-            index={index}
+            index={page ? (page - 1) * LINKS_PER_PAGE + index : index}
             link={link}
+            updateStoreAfterVote={this._updateCacheAfterVote}
           />
         ))}
+        </div>
+        {isNewPage &&
+        <div className='flex ml4 mv3 gray'>
+          <div className='pointer mr2' onClick={() => this._previousPage()}>Previous</div>
+          <div className='pointer' onClick={() => this._nextPage()}>Next</div>
+        </div>
+        }
       </div>
     )
+  }
+
+  _getLinksToRender = (isNewPage) => {
+    if (isNewPage) {
+      return this.props.allLinksQuery.allLinks
+    }
+    const rankedLinks = this.props.allLinksQuery.allLinks.slice()
+    rankedLinks.sort((l1, l2) => l2.votes.length - l1.votes.length)
+    return rankedLinks
+  }
+
+  _nextPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page <= this.props.allLinksQuery._allLinksMeta.count / LINKS_PER_PAGE) {
+      const nextPage = page + 1
+      this.props.history.push(`/new/${nextPage}`)
+    }
+  }
+
+  _previousPage = () => {
+    const page = parseInt(this.props.match.params.page, 10)
+    if (page > 1) {
+      const previousPage = page - 1
+      this.props.history.push(`/new/${previousPage}`)
+    }
   }
 
   _subscribeToNewLinks = () => {
@@ -123,7 +159,12 @@ class LinkList extends Component {
   }
 
   _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: ALL_LINKS_QUERY })
+    const isNewPage = this.props.location.pathname.includes('new')
+    const page = parseInt(this.props.match.params.page, 10)
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+    const first = isNewPage ? LINKS_PER_PAGE : 100
+    const orderBy = isNewPage ? 'createdAt_DESC' : null
+    const data = store.readQuery({ query: ALL_LINKS_QUERY, variables: {first, skip, orderBy} })
     const votedLink = data.allLinks.find(link => link.id === linkId)
     votedLink.votes = createVote.link.votes
     store.writeQuery({ query: ALL_LINKS_QUERY, data })
@@ -131,8 +172,8 @@ class LinkList extends Component {
 }
 
 export const ALL_LINKS_QUERY = gql`
-  query AllLinksQuery {
-    allLinks {
+  query AllLinksQuery($first: Int, $skip: Int, $orderBy: LinkOrderBy) {
+    allLinks(first: $first, skip: $skip, orderBy: $orderBy) {
       id
       createdAt
       url
@@ -148,7 +189,22 @@ export const ALL_LINKS_QUERY = gql`
         }
       }
     }
+    _allLinksMeta {
+      count
+    }
   }
 `
 
-export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList)
+export default graphql(ALL_LINKS_QUERY, {
+  name: 'allLinksQuery',
+  options: (ownProps) => {
+    const page = parseInt(ownProps.match.params.page, 10)
+    const isNewPage = ownProps.location.pathname.includes('new')
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+    const first = isNewPage ? LINKS_PER_PAGE : 100
+    const orderBy = isNewPage ? 'createdAt_DESC' : null
+    return {
+      variables: { first, skip, orderBy }
+    }
+  }
+})(LinkList)
